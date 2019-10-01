d716dddb6ec6dd586cc361cb0673dc9b1642e849
ThanhPhong(PhongVo) <phongvo2728@gmail.com>
2019-10-01 17:33:36 +0700

    Excercise04

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -6,10 +6,35 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+        }
+        // input: float x
+        // output: int a that is smallest but not smaller than x
+        // idea: a = (int) x then a = a+1
+        // idea: if x is negative then a is unchanged
+        public static int Ceil(float x)
+        {
+            int a = (int)x;
+            if (x > 0)
+                if (x > a) 
+                    a = a + 1;
+            return a;
+        }
+        // input: float x
+        // output: int a that is largest but not bigger than x
+        // idea: a= (int)x 
+        // if x negative then a=a-1
+        // if x = a then a is unchanged
+        public static int Floor(float x)
+        {
+            int a = (int)x;
+            if (x < 0)
+                if (x != a)
+                    a = a - 1;
+            return a;
         }
     }
 }
---
+++ ConsoleApplication1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1; 
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCeil()
+        {
+            int result1 = Program.Ceil(2.3f);
+            Assert.AreEqual(3, result1);
+
+            int result2 = Program.Ceil(-2.3f);
+            Assert.AreEqual(-2, result2);
+
+            int result3 = Program.Ceil(3);
+             Assert.AreEqual(3,result3);
+
+
+        }
+        [TestMethod]
+        public void TestFloor()
+        {
+            int result1 = Program.Floor(2.3f);
+            Assert.AreEqual(2, result1);
+
+            int result2 = Program.Floor(-3.4f);
+            Assert.AreEqual(-4, result2);
+
+            int result3 = Program.Floor(5);
+            Assert.AreEqual(5, result3);
+        }
+    }
+}
